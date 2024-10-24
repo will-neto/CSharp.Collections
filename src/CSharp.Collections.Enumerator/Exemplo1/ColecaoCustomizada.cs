e06dd7411b04f880178e4c1f78d5cd1f5e0f9fc5
Wilson dos Santos Alberto <will.neto@outlook.com>
2024-10-23 23:18:58 -0300

    Add project files.

---
+++ src/CSharp.Collections.Enumerator/Exemplo1/ColecaoCustomizada.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Collections.IEnumerator.Exemplo1
+{
+    // Classe customizada de coleção
+    internal class ColecaoCustomizada<T>
+    {
+        private List<T> items = new List<T>();
+
+        public ColecaoCustomizada()
+        {
+            
+        }
+
+        public ColecaoCustomizada(params T[] numeros)
+        {
+            items = new List<T>(numeros);
+        }
+
+        public void Adicionar(T obj)
+        {
+            items.Add(obj);
+        }
+
+        public void Limpar()
+        {
+            items = new List<T>();
+        }
+
+        public void Remover(T obj)
+        {
+            items.Remove(obj);
+        }
+
+        // Para o funcionamento no foreach, é necessário possui um método publico com nome GetEnumerator() e que retorne um
+        // objeto que contenha a implementação dos métodos MoveNext(), Reset() e declaração da propriedade Current
+        public EnumeradorCustomizado GetEnumerator()
+        {
+
+            return new EnumeradorCustomizado(this);
+        }
+
+        // Classe que é retornada pelo método GetEnumerator na coleção customizada
+        public class EnumeradorCustomizado
+        {
+            private readonly ColecaoCustomizada<T> _colecao;
+            private int _indice = -1;
+
+            public EnumeradorCustomizado(ColecaoCustomizada<T> colecao)
+            {
+                _colecao = colecao;
+            }
+
+            // Obrigatório ter a propriedade declarada para funcionar corretamente no foreach
+            public T Current => _colecao.items[_indice];
+
+            // Obrigatório ter a implementação do método para funcionar corretamente no foreach
+            public bool MoveNext()
+            {
+                _indice++;
+                return _indice < _colecao.items.Count;
+            }
+
+            // Obrigatório ter a implementação do método para funcionar corretamente no foreach
+            public void Reset()
+            {
+                _indice = -1;
+            }
+        }
+    }
+}
---
+++ src/CSharp.Collections.Enumerator/Program.cs
@@ -0,0 +1,72 @@
+﻿using CSharp.Collections.IEnumerator.Exemplo1;
+using System.Collections;
+
+namespace CSharp.Collections.I0Enumerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Enumerator();
+
+            ColecaoCustomizadaExemplo();
+        }
+
+
+        private static void Enumerator()
+        {
+            /*
+               - Interface é usada para permitir o looping via foreach em classes que utilizam a mesma. 
+               Internamente, o "foreach" utiliza o GetEnumerator() para seu funcionamento
+
+               - Um exemplo são as interfaces IEnumerable e IEnumerable<T>, ambas possuem a declaração do método GetEnumerator,
+               que retorna um IEnumerator
+            */
+
+            IEnumerable<int> numeros = new List<int>() { 10, 2, 3, 5 };
+            IEnumerable caracteres = new ArrayList { 11, "teste", 2L, 3F, 1D };
+            IEnumerable<int> numeros2 = new Queue<int>(new int[] { 12, 2, 3, 4, 5 });
+
+            // Possui o método GetEnumerator() com implementação através de cada um dos tipos concretos
+
+            var numerosEnumerador = numeros.GetEnumerator();
+            var caracteressEnumerador = caracteres.GetEnumerator();
+            var numeros2Enumerador = numeros2.GetEnumerator();
+
+            // As classes concretas que implementam o GetEnumerator() retornam uma instancia nova sempre que chamado o método
+            // Algumas podem disparar erros caso não seja chamado o método MoveNext();
+            numeros.GetEnumerator().MoveNext();
+            caracteressEnumerador.MoveNext();
+            numeros2Enumerador.MoveNext();
+
+            Console.WriteLine("numeros (List<int>) = " + numerosEnumerador.Current); // Não dispara erro se nao chamado MoveNext() antes de usar Current
+            Console.WriteLine("caracteres (ArrayList) = " + caracteressEnumerador.Current); // Dispara erro se nao chamado MoveNext() antes de usar Current
+            Console.WriteLine("numeros (Queue<int>) = " + numeros2Enumerador.Current); // Dispara erro se nao chamado MoveNext() antes de usar Current
+
+            numerosEnumerador.MoveNext();
+            Console.WriteLine("numeros (List<int>) = " + numerosEnumerador.Current); // Não dispara erro se nao chamado MoveNext() antes de usar Current
+
+        }
+
+
+        private static void ColecaoCustomizadaExemplo()
+        {
+            ColecaoCustomizada<int> colecaoCustomizada = new ColecaoCustomizada<int>(1,2,3,4,5,6);
+
+            // Percorre a instância da classe de coleção customizada devido a implementação dos métodos necessários que permitem
+            // tal abordagem
+            Console.WriteLine("\n\nColeção Customizada com implementação de GetEnumerator()");
+            foreach(var item in colecaoCustomizada)
+            {
+                Console.Write(item + " | ");
+            }
+
+            // Não é possível utilizar a instância em um loop for devido a classe não ter sido indexada
+            // Exemplo de indexação no CSharp.Collections.Indexador
+            //for (var i = 0; i < 10; i++)
+            //{
+            //    colecaoCustomizada[i];
+            //}
+        }
+    }
+}
